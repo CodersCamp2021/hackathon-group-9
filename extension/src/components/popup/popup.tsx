e140e556085654538fcc61a64354d637056c3b2b
Dawid Gębara <gebara.dawid00@gmail.com>
2022-06-05 01:18:17 +0200

    finish popup

--- extension/src/components/popup/popup.tsx
+++ extension/src/components/popup/popup.tsx
@@ -3,58 +3,107 @@ import { useState, useEffect } from "react";
 import  { ImArrowUp, ImArrowDown } from "react-icons/im";
 import { Button } from "@nextui-org/react";
 
-
-
-export type SelectedFlag = "red" | "yellow" | "green";
-
 const Popup = () => {
-    const [selectedFlag, setSelectedFlag] = useState<SelectedFlag>("green");
-    const [opinions, setOpinions] = useState<{positive: number, negative: number}>({
-        positive: 2,
-        negative: 3
-    });
-    const [domainInfo, setDomainInfo] = useState({
-        domainOwner: "dupa1234",
-        registeredCountry: "Russia",
-        creationDate: "22.01.1945"
-    })
+    const [currentDomain, setCurrentDomain] = useState<any>(null);
+    const [opinions, setOpinions] = useState<any>(null);
+    const [domainInfo, setDomainInfo] = useState<any>(null)
+
+    const addingDomainHandler = async () => {
+        const data = await fetch(`https://earnest-palmier-0fac39.netlify.app/api/pages`, {
+            method: "POST",
+            headers: {"content-type": "application/json"},
+            body: JSON.stringify({url: currentDomain})
+        });
+        const parsedData = await data.json();
+        setOpinions(parsedData);
+    }
+
+    const upvoteHandler = async () => {
+        const response = await fetch(`https://earnest-palmier-0fac39.netlify.app/api/pages/${encodeURIComponent(currentDomain)}/upvote`, {
+            method: "POST",
+        });
+        const data = await response.json()
+        setOpinions(data)
+    }
+
+    const downvoteHandler = async () => {
+        console.log('down')
+        const response = await fetch(`https://earnest-palmier-0fac39.netlify.app/api/pages/${encodeURIComponent(currentDomain)}/downvote`, {
+            method: "POST",
+        });
+        const data = await response.json()
+        setOpinions(data)
+    }
 
     useEffect(() => {
-        const fetchData = () => {
+        const fetchData = async () => {
+            const [tab] = await chrome.tabs.query({
+                active: true,
+                currentWindow: true,
+              });
 
-        }
+              setCurrentDomain(tab.url)
+        };
+
+        fetchData();
     }, [])
 
+    useEffect(() => {
+        if(currentDomain) {
+            const fetchDataFromServer = async () => {
+                const response = await fetch(`https://earnest-palmier-0fac39.netlify.app/api/pages/${encodeURIComponent(currentDomain)}`, {
+                    method: "GET"
+                });
+                if(response.ok) {
+                    const data = await response.json()
+                    setOpinions(data)
+                }
+
+                const response2 = await fetch(`https://earnest-palmier-0fac39.netlify.app/api/pages/${encodeURIComponent(currentDomain)}/whois`);
+                const domainInfo = await response2.json();
+                console.log("🚀 ~ file: popup.tsx ~ line 64 ~ fetchDataFromServer ~ domainInfo", domainInfo);
+                setDomainInfo(domainInfo);
+            }
+
+            fetchDataFromServer();
+        }
+    }, [currentDomain])
+
     return (
-        <PopupWrapper>
-            <FlagWrapper selectedFlag={selectedFlag} />
-            <ArrowsWrapper>
-                <span>{opinions.negative}</span>
-                <Button auto flat color="error" icon={<ImArrowDown style={{transform: "scale(1.5)"}}/>}/>
-                <Button auto flat color="success" icon={<ImArrowUp style={{transform: "scale(1.5)"}} />} />
-                <span>{opinions.positive}</span>
-            </ArrowsWrapper>
-            <p>
-                <span>
-                    Domain owner: {domainInfo.domainOwner || "No data available"}
-                </span>
-                <span>
-                    country: {domainInfo.registeredCountry || "No data available"}
-                </span>
-                <span>
-                    Creation date: {domainInfo.creationDate || "No data available"}
-                </span>
-            </p>
-            <Footer>
-                <Button 
-                    as="a" 
-                    href="https://earnest-palmier-0fac39.netlify.app/" 
-                    target="_blank" 
-                    size="xs" 
-                    style={{textDecoration: "none"}}>
-                        Show More
-                    </Button>
-            </Footer>
+        <PopupWrapper opinions={opinions}>
+            {opinions ? 
+            <>
+                <FlagWrapper selectedFlag={opinions.flag} />
+                <ArrowsWrapper>
+                    <span>{opinions.downvotes}</span>
+                    <Button auto flat color="error" onPress={downvoteHandler} icon={<ImArrowDown style={{transform: "scale(1.5)"}} />}/>
+                    <Button auto flat color="success" onPress={upvoteHandler} icon={<ImArrowUp style={{transform: "scale(1.5)"}} />} />
+                    <span>{opinions.upvotes}</span>
+                </ArrowsWrapper>
+                <p>
+                    <span>
+                        Domain owner: {domainInfo?.owner || "No data available"}
+                    </span>
+                    <span>
+                        Country: {domainInfo?.registeredCountry || "No data available"}
+                    </span>
+                    <span>
+                        Creation date: {domainInfo?.created?.split(" ")?.[0].slice(0, 11).replace("T", "") || "No data available"}
+                    </span>
+                </p>
+                <Footer>
+                    <Button 
+                        as="a" 
+                        href="https://earnest-palmier-0fac39.netlify.app/" 
+                        target="_blank" 
+                        size="xs" 
+                        style={{textDecoration: "none"}}>
+                            Show More
+                        </Button>
+                </Footer>
+            </> : 
+            <Button onClick={addingDomainHandler}>Add current domain!</Button>
+            }
         </PopupWrapper>
     )
 }
