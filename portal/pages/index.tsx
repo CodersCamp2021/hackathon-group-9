9ffc02533345477df1a8a94638505d74f428acb7
Rysiek <dpietruszewski12@gmail.com>
2022-06-04 13:45:36 +0200

    Add example data

--- portal/pages/index.tsx
+++ portal/pages/index.tsx
@@ -4,6 +4,31 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
+const data = [
+ {
+    id: "1234",
+    createdAt: "12:23",
+    url: "test.com",
+    upvotes: "21",
+    downvotes: "32"
+  },
+
+  {
+    id: "1233424",
+    createdAt: "13:23",
+    url: "test12.com",
+    upvotes: "221",
+    downvotes: "321"
+  },
+  {
+    id: "1243234",
+    createdAt: "13:23",
+    url: "test123.com",
+    upvotes: "24",
+    downvotes: "72"
+  }
+]
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
