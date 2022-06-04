00087c625c94a75f4d73026fb13c85268ff9e67a
Maciej Mroczko <maciej.mroczko@o2.pl>
2022-06-04 22:44:55 +0200

    Create modal and add api services

--- portal/components/item.tsx
+++ portal/components/item.tsx
@@ -1,33 +1,47 @@
-import * as React from "react";
+import React from "react";
 import styles from "../styles/Item.module.css";
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
 
 type ItemProps = {
-    time: string
-    url: string
-    upvotes: string
-    downvotes: string
+  time: string;
+  url: string;
+  upvotes: number;
+  downvotes: number;
+  onUpVote: VoidFunction;
+  onDownVote: VoidFunction;
+};
+
+function Item({
+  time,
+  url,
+  upvotes,
+  downvotes,
+  onUpVote,
+  onDownVote,
+}: ItemProps) {
+  return (
+    <div className={styles.item}>
+      <p className={styles.time}>{time}</p>
+      <p className={styles.url}>{url}</p>
+      <div className={styles.votesWrapper}>
+        <div className={styles.vote}>
+          <button className={styles.voteBtn} onClick={onUpVote}>
+            <Icon icon="cil:arrow-thick-top" className={styles.upVoteArrow} />
+          </button>
+          <p className={styles.upvotes}>{upvotes}</p>
+        </div>
+        <div className={styles.vote}>
+          <button className={styles.voteBtn} onClick={onDownVote}>
+            <Icon
+              icon="cil:arrow-thick-bottom"
+              className={styles.downVoteArrow}
+            />
+          </button>
+          <p className={styles.downvotes}>{downvotes}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-function Item({time, url, upvotes, downvotes}: ItemProps) {
-
-    return (
-
-                <div className={styles.item}>
-                    <p className={styles.time}>{time}</p>
-                    <p className={styles.url}>{url}</p>
-                    <div className={styles.votesWrapper}>
-                        <div className={styles.vote}>
-                            <Icon icon="cil:arrow-thick-top" className={styles.upVoteArrow} />
-                            <p className={styles.upvotes}>{upvotes}</p>
-                        </div>
-                        <div className={styles.vote}>
-                            <Icon icon="cil:arrow-thick-bottom" className={styles.downVoteArrow} />
-                            <p className={styles.downvotes}>{downvotes}</p>
-                        </div>
-                    </div>
-                </div>
-    )
-}
-
-export default Item
+export default Item;
--- portal/pages/index.tsx
+++ portal/pages/index.tsx
@@ -1,83 +1,133 @@
-import React, {useState} from "react"
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Home.module.css";
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-import Item from '../components/item'
-import { Icon } from '@iconify/react';
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Item from "../components/item";
+import { Icon } from "@iconify/react";
 import Modal from "../components/modal";
+import { Page as DBPage } from "@prisma/client";
+import { getPages } from "../services";
 
-const data = [
- {
-    id: "1234",
-    createdAt: "12:23",
-    url: "test.com",
-    upvotes: "21",
-    downvotes: "32"
-  },
-
-  {
-    id: "1233424",
-    createdAt: "13:23",
-    url: "test12.com",
-    upvotes: "221",
-    downvotes: "321"
-  },
-  {
-    id: "1243234",
-    createdAt: "13:23",
-    url: "test123.com",
-    upvotes: "24",
-    downvotes: "72"
-  }
-]
+type Page = Omit<DBPage, "createdAt"> & {
+  createdAt: string;
+};
 
 const Home = () => {
-  const [webUrl, setWebUrl] = useState('')
-  const [isOpen, setIsOpen] = useState(false)
+  const [webUrl, setWebUrl] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [pages, setPages] = useState<Page[]>([]);
+  const [submitUrl, setSubmitUrl] = useState("");
 
-  const filtered = data.filter((item) => item.url === webUrl)
-  const items = filtered.length ? filtered : data
+  const filtered = pages.filter((item) => item.url.includes(webUrl));
+  const items = filtered.length ? filtered : pages;
+
+  async function callUpvote(url: string) {
+    const encodedURL = encodeURIComponent(url);
+    const res = await fetch(`/api/pages/${encodedURL}/upvote`, {
+      method: "POST",
+    });
+    if (res.ok) {
+      const page = (await res.json()) as Page;
+      setPages(pages.filter((p) => p.id !== page.id).concat(page));
+    }
+  }
+
+  async function callDownvote(url: string) {
+    const encodedURL = encodeURIComponent(url);
+    const res = await fetch(`/api/pages/${encodedURL}/downvote`, {
+      method: "POST",
+    });
+    if (res.ok) {
+      const page = (await res.json()) as Page;
+      setPages(pages.filter((p) => p.id !== page.id).concat(page));
+    }
+  }
+
+  async function submitPage(submittedUrl: string) {
+    const res = await fetch("/api/pages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        url: submittedUrl,
+      } as Partial<Page>),
+    });
+    if (res.ok) {
+      const page = (await res.json()) as Page;
+      setPages(pages.filter((p) => p.id !== page.id).concat(page));
+      setIsOpen(false);
+      setSubmitUrl("");
+    }
+  }
+
+  useEffect(() => {
+    getPages().then(setPages);
+  }, []);
 
   return (
     <>
-    
-    <Modal isOpen={isOpen}  onClose={() => setIsOpen(false)}>
-      <h1 className={styles.header}>Report a website</h1>
-      <TextField fullWidth label="URL" id="fullWidth" />
-      <div className={styles.btnContainer}>
-        <Button variant="contained">Submit</Button>
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <h1 className={styles.header}>Report a website</h1>
+        <TextField
+          fullWidth
+          label="URL"
+          id="fullWidth"
+          onChange={(e) => setSubmitUrl(e.target.value)}
+        />
+        <div className={styles.btnContainer}>
+          <Button variant="contained" onClick={() => submitPage(submitUrl)}>
+            Submit
+          </Button>
+        </div>
+      </Modal>
+
+      <div className={styles.container}>
+        <button className={styles.plusBtn} onClick={() => setIsOpen(true)}>
+          <Icon icon="akar-icons:circle-plus-fill" className={styles.addIcon} />
+        </button>
+        <Box
+          sx={{
+            width: 500,
+            maxWidth: "100%",
+          }}
+        >
+          <TextField
+            fullWidth
+            label="Search"
+            id="fullWidth"
+            onChange={(e) => setWebUrl(e.target.value)}
+            type="url"
+          />
+        </Box>
       </div>
-    </Modal>
 
-    <div className={styles.container}>
-    <button className={styles.plusBtn} onClick={() => setIsOpen(true)}>
-        <Icon icon="akar-icons:circle-plus-fill" className={styles.addIcon} />
-    </button>
-    <Box
-    sx={{
-      width: 500,
-      maxWidth: '100%',
-    }}
-  >
-    <TextField fullWidth label="Search" id="fullWidth" onChange={e => setWebUrl(e.target.value)} />
-  </Box>
-  <Button variant="text">Search</Button>
-  </div>
-
-<div className={styles.items}>
-  <div className={styles.itemContainer}>
-      {items.map((el) => {
-        return(
-          <div className={styles.itemWrapper} key={el.url}>
-            <Item  time={el.createdAt} url={el.url} upvotes={el.upvotes} downvotes={el.downvotes} ></Item>
-          </div>
-        )
-      })} 
-  </div>
- </div>
-  
-  </>
+      <div className={styles.items}>
+        <div className={styles.itemContainer}>
+          {items
+            .sort((a, b) => Number(new Date(b.createdAt)) - Number(new Date(a.createdAt)))
+            .map((el) => {
+              return (
+                <div className={styles.itemWrapper} key={el.url}>
+                  <Item
+                    onUpVote={() => {
+                      callUpvote(el.url);
+                    }}
+                    onDownVote={() => {
+                      callDownvote(el.url);
+                    }}
+                    time={el.createdAt.slice(0, -14)}
+                    url={el.url}
+                    upvotes={el.upvotes}
+                    downvotes={el.downvotes}
+                  ></Item>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </>
   );
 };
 
